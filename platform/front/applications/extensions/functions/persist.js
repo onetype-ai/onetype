830902f90cf82887db42169708a9a7221550329b
Dejan Tomic <hi@iamdejan.com>
2026-07-10 15:06:37 +0200

    platform/front: remove extensions app

--- platform/front/applications/extensions/functions/persist.js
+++
@@ -1,6 +0,0 @@
-extensions.Fn('persist', function()
-{
-	const installed = Object.values(this.Items()).filter((item) => item.Get('installed')).map((item) => item.Get('id'));
-
-	$ot.settings.set('extensions.installed', installed);
-});
