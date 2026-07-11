8e3c250e7f59e00cea966d2766333206763eb1a0
Dejan Tomic <hi@iamdejan.com>
2026-07-11 13:30:12 +0200

    data: filters depth 1 default

--- runtimes/home/front/items/elements/data/filters/filters.js
+++ runtimes/home/front/items/elements/data/filters/filters.js
@@ -92,8 +92,9 @@ onetype.AddonReady('elements', (elements) =>
 			},
 			background: {
 				type: 'number',
+				value: 1,
 				options: [1, 2, 3],
-				description: 'Background depth of the panel surface from 1 to 3. Empty renders it bare.'
+				description: 'Background depth of the panel surface from 1 to 3.'
 			},
 			_change: {
 				type: 'function',
